1b2cd2c0c4e4f6a2bb98b49c55be85a40ba60caa
priyankamackwan <priyu08@gmail.com>
2022-02-21 19:02:55 +0530

    add register, login, profile page

--- index.js
+++ index.js
@@ -15,6 +15,26 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+app.get('/login', function(req, res) {
+  res.render('auth/login');
+});
+
+app.get('/register', function(req, res) {
+  res.render('auth/register');
+});
+
+app.get('/contact', function(req, res) {
+  res.render('pages/contact');
+});
+
+app.get('/users-profile', function(req, res) {
+  res.render('pages/profile');
+});
+app.get('/pages-faq', function(req, res) {
+  res.send('Coming Soon');
+});
+
+
 app.listen(PORT, () => {
   console.log('Server connected at:',PORT);
 });
